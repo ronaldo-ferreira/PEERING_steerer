aa9ec8375eb1e3d71d68e74f6a66f5f5a0e84266
Ronaldo Alves Ferreira <raf@facom.ufms.br>
2017-12-26 11:26:38 -0300

    adding upstream interfaces for routing packets coming out of the container

--- user/create_container_isolation.py
+++ user/create_container_isolation.py
@@ -77,8 +77,39 @@ def mb_config(container_name, iface_prefix, ip_prefix, con0_ip, con1_ip, con2_ip
     Popen("ip netns exec %s ip link set veth-raf0 up" % ns_mux, shell=True).wait()
     Popen("ip netns exec %s ip link set veth-raf1 up" % ns_mux, shell=True).wait()
 
-    # Add a rule to lookup table 10000 for packets coming from the container to upstream
+    # The interfaces below should be configured from a file (interface-container.json, for example). We should
+    # reuse the interface.py code. The IP addresses are the same as the ones in the file interface.json, but
+    # we rewrite the third octect from 0 to 192.
+    Popen("ip netns exec %s ip link add upstream1b link veth-raf0 type macvlan" % ns_mux, shell=True).wait() 
+    Popen("ip netns exec %s ip link add upstream2b link veth-raf0 type macvlan" % ns_mux, shell=True).wait() 
+    Popen("ip netns exec %s ip link add upstream4b link veth-raf0 type macvlan" % ns_mux, shell=True).wait() 
+    Popen("ip netns exec %s ip link add upstream5b link veth-raf0 type macvlan" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip link add upstream6b link veth-raf0 type macvlan" % ns_mux, shell=True).wait()
+
+    Popen("ip netns exec %s ip addr add 100.65.192.1/24 dev upstream1b" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip addr add 100.65.192.2/24 dev upstream2b" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip addr add 100.65.192.4/24 dev upstream4b" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip addr add 100.65.192.5/24 dev upstream5b" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip addr add 100.65.192.6/24 dev upstream6b" % ns_mux, shell=True).wait()
+
+    # Add the mirror interfaces for the upstream peers. These interfaces are used to route
+    # packets coming out of the container. This configuration should be done using interfaces.json or
+    # a separate file with only these interfaces.
+    Popen("ip netns exec %s ip link set upstream1b up" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip link set upstream2b up" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip link set upstream4b up" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip link set upstream5b up" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip addr set upstream6b up" % ns_mux, shell=True).wait()
+    
+    
+    # Add rules to lookup the upstream tables for packets coming from the container enroute to upstream.
+    # These rules should be configured from the information in the database.
     Popen("ip netns exec %s ip rule add iif veth-raf0 table 10000 priority 10000" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip rule add iif upstream1b table 10001 priority 10001" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip rule add iif upstream2b table 10002 priority 10002" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip rule add iif upstream4b table 10004 priority 10004" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip rule add iif upstream5b table 10005 priority 10005" % ns_mux, shell=True).wait()
+    Popen("ip netns exec %s ip rule add iif upstream6b table 10006 priority 10006" % ns_mux, shell=True).wait()
     
     # Transfer interfaces to MB container
     Popen("ip netns exec %s ip link set %s netns %s up" % (ns_mux, con1, ns_mb), shell=True).wait()
@@ -91,6 +122,9 @@ def mb_config(container_name, iface_prefix, ip_prefix, con0_ip, con1_ip, con2_ip
 
     # Configure routes
     Popen("docker exec -it %s ip route add %s via %s" % (container_name, ip_prefix, con0_ip), shell=True).wait()
+
+    # The default route below should commented out if we configure an iBGP session between the client and
+    # the container.
     Popen("docker exec -it %s ip route add default via %s" % (container_name, con2_ip), shell=True).wait()
     
 
@@ -106,8 +140,8 @@ def main(argv):
     ip_prefix = "184.164.224.128/25"
     con0_ip = "100.65.128.2" 
     con1_ip = "100.65.128.254"
-    con2_ip = "100.65.192.1"   
-    con3_ip = "100.65.192.2"
+    con2_ip = "100.65.192.254"   
+    con3_ip = "100.65.192.253"
     mb_config(container_name, iface_prefix, ip_prefix, con0_ip, con1_ip, con2_ip, con3_ip)
 
     
