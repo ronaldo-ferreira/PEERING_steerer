7eff430c28ae69f22fe9264784216930a4a10de1
Brivaldo Junior <brivaldo@facom.ufms.br>
2018-01-26 11:40:48 -0300

    Solving some specific problems.

--- django/dockerweb/web/ssh.py
+++ django/dockerweb/web/ssh.py
@@ -6,7 +6,7 @@ class SSH:
         self.ssh = SSHClient()
         self.ssh.load_system_host_keys()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.ssh.connect(hostname='10.87.7.130',username='brivaldo',password='br1v@$jr')
+        self.ssh.connect(hostname='10.87.7.130',username='brivaldo',password='')
  
     def exec_cmd(self,cmd):
         stdin,stdout,stderr = self.ssh.exec_command(cmd)
